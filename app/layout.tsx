dca58e4f31befa2519331cf9e4ab4bd543129f59
Danny <danobird1@gmail.com>
2025-05-02 20:28:46 -0700

    Add Farcaster frame meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   description: "A template for building mini-apps on Farcaster and Monad",
   other: {
     'fc:frame': 'vNext',
-    'fc:frame:image': 'https://my-fitness-miniapp.vercel.app/preview.png.png',
+    'fc:frame:image': 'https://cdn.prod.website-files.com/667c57e6f9254a4b6d914440/667f1590ccceec3eee19ec7c_Favicon.png',
     'fc:frame:button:1:text': 'Open MiniApp',
     'fc:frame:button:1:url': 'https://my-fitness-miniapp.vercel.app',
   },
